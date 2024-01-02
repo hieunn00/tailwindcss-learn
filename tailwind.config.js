3618368ddde1d58c64a06b2f00f35af34a9336a6
hieunn00 <154812500+hieunn00@users.noreply.github.com>
2024-01-02 14:39:43 +0700

    product-mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,15 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
+        },
+        fadeIn: {
+          from:{opacity: 0},
+          to: {opacity: 1}
         }
       },
       animation: {
-        slideDown: 'slideDown 0.4s ease-in-out'
+        slideDown: 'slideDown 0.5s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out'
       }
     },
   },
